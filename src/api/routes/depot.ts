abd6891652a96d2a4e75fcf3952eec28373978ca
Talv <krz.krawczuk@gmail.com>
2020-08-23 05:06:29 +0200

    webapi: new depot endpoint for all kinds of images, not just map icons

--- src/api/routes/depot.ts
+++ src/api/routes/depot.ts
@@ -10,11 +10,34 @@ const bnDepot = new BattleDepot('data/depot');
 const pubBnetDir = new NestedHashDir('data/bnet');
 
 export default fp(async (server, opts, next) => {
+    async function getDepotImage(region: GameRegion, hash: string) {
+        const jpgPath = pubBnetDir.pathTo(`${hash}.jpg`);
+        if (!(await fs.pathExists(jpgPath))) {
+            const s2mvPath = await bnDepot.getPathOrRetrieve(
+                GameRegion[region].toLowerCase(),
+                `${hash}.s2mv`
+            );
+            await convertImage(s2mvPath, jpgPath, ['-format', 'jpg', '-quality', '80', '-strip']);
+            await fs.unlink(s2mvPath);
+        }
+        return jpgPath;
+    }
+
     server.get('/bnet/:hash(^\\w+).jpg', {
         config: {
             rateLimit: {
                 max: 2000,
                 timeWindow: 1000 * 60 * 10,
+            },
+        },
+        schema: {
+            params: {
+                type: 'object',
+                properties: {
+                    hash: {
+                        type: 'string',
+                    },
+                },
             },
         },
     }, async (request, reply) => {
@@ -31,12 +54,7 @@ export default fp(async (server, opts, next) => {
                     return reply.callNotFound();
                 }
 
-                const s2mvPath = await bnDepot.getPathOrRetrieve(
-                    GameRegion[result.regionId].toLowerCase(),
-                    `${request.params.hash}.s2mv`
-                );
-                await convertImage(s2mvPath, jpgPath, ['-format', 'jpg', '-quality', '85', '-strip']);
-                await fs.unlink(s2mvPath);
+                await getDepotImage(result.regionId, request.params.hash);
             }
             catch (err) {
                 logger.error(err);
@@ -45,5 +63,38 @@ export default fp(async (server, opts, next) => {
         }
         reply.header('Cache-control', 'public, max-age=604800, s-maxage=604800');
         return reply.sendFile(jpgPath, path.resolve('.'));
+    });
+
+    server.get('/depot/:region/:hash(^\\w+).jpg', {
+        config: {
+            rateLimit: {
+                max: 2000,
+                timeWindow: 1000 * 60 * 10,
+            },
+        },
+        schema: {
+            params: {
+                type: 'object',
+                properties: {
+                    region: {
+                        type: 'string',
+                        enum: ['us', 'eu', 'kr'],
+                    },
+                    hash: {
+                        type: 'string',
+                    },
+                },
+            },
+        },
+    }, async (request, reply) => {
+        try {
+            const jpgPath = await getDepotImage((GameRegion as any)[request.params.region.toUpperCase()], request.params.hash);
+            reply.header('Cache-control', 'public, max-age=604800, s-maxage=604800');
+            return reply.sendFile(jpgPath, path.resolve('.'));
+        }
+        catch (err) {
+            logger.error(err);
+            return reply.code(500);
+        }
     });
 });
