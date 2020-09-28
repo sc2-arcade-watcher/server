24e5609e85925ddbd0b4036fe7f270245f5f38f8
Talv <krz.krawczuk@gmail.com>
2020-09-28 03:34:49 +0200

    reformat ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,30 +1,32 @@
 require('dotenv').config();
 
+// /** @type {import('typeorm/connection/ConnectionOptions').ConnectionOptions} */
+/** @type {import('typeorm/driver/mysql/MysqlConnectionOptions').MysqlConnectionOptions} */
 module.exports = {
-    "type": process.env.SQL_TYPE,
-    "host": "localhost",
-    "port": 3306,
-    "username": process.env.SQL_USER,
-    "password": process.env.SQL_PASS,
-    "database": process.env.SQL_DATABASE,
-    "charset": "utf8mb4",
-    "timezone": "+00:00",
-    "synchronize": false,
-    "logging": Boolean(process.env.SQL_LOGGING || false),
-    "namingStrategy": new (require('typeorm-naming-strategies').SnakeNamingStrategy)(),
-    "entities": [
-        "out/src/entity/**/*.js"
+    type: process.env.STARC_SQL_TYPE,
+    host: process.env.STARC_SQL_HOST,
+    port: Number(process.env.STARC_SQL_PORT),
+    username: process.env.STARC_SQL_USER,
+    password: process.env.STARC_SQL_PASS,
+    database: process.env.STARC_SQL_DATABASE,
+    charset: 'utf8mb4',
+    timezone: '+00:00',
+    synchronize: false,
+    logging: process.env.STARC_SQL_LOGGING || 'error',
+    namingStrategy: new (require('typeorm-naming-strategies').SnakeNamingStrategy)(),
+    entities: [
+        'out/src/entity/**/*.js'
     ],
-    "migrations": [
-        "out/src/migration/**/*.js"
+    migrations: [
+        'out/src/migration/**/*.js'
     ],
-    "subscribers": [
-        "out/src/subscriber/**/*.js"
+    subscribers: [
+        'out/src/subscriber/**/*.js'
     ],
-    "supportBigNumbers": true,
-    "bigNumberStrings": true,
-    "cache": {
-        "type": "database",
-        "tableName": "tmp_query_cache",
+    supportBigNumbers: true,
+    bigNumberStrings: true,
+    cache: {
+        type: 'database',
+        tableName: 'tmp_query_cache',
     },
 };
